633dccf600343a9fb88b297dde713b3293ba22e5
Nandocho8 <ccontrerasvergara@hotmail.cl>
2023-05-11 18:57:05 -0400

    cambio de cors en main y eliminacion cors.go

--- cors.go
+++ cors.go
@@ -1,44 +1,44 @@
 package main
 
-import (
-	"net/http"
+// import (
+// 	"net/http"
 
-	"github.com/rs/cors"
-)
+// 	"github.com/rs/cors"
+// )
 
-func handleOptions(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT")
-	w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.WriteHeader(http.StatusOK)
-}
+// func handleOptions(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Access-Control-Allow-Origin", "*")
+// 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT")
+// 	w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+// 	w.Header().Set("Access-Control-Allow-Credentials", "true")
+// 	w.WriteHeader(http.StatusOK)
+// }
 
-func corsMiddleware(handleFunc http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Verificar si es una solicitud OPTIONS
-		if r.Method == "OPTIONS" {
-			handleOptions(w, r)
-			return
-		}
+// func corsMiddleware(handleFunc http.HandlerFunc) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		// Verificar si es una solicitud OPTIONS
+// 		if r.Method == "OPTIONS" {
+// 			handleOptions(w, r)
+// 			return
+// 		}
 
-		// Configurar las opciones CORS
-		c := cors.New(cors.Options{
-			AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-			AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-			Debug:          true,
-		})
+// 		// Configurar las opciones CORS
+// 		c := cors.New(cors.Options{
+// 			AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+// 			AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+// 			Debug:          true,
+// 		})
 
-		// Aplicar las opciones CORS a la solicitud
-		handler := c.Handler(handleFunc)
+// 		// Aplicar las opciones CORS a la solicitud
+// 		handler := c.Handler(handleFunc)
 
-		// Agregar los encabezados CORS a la respuesta HTTP
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT")
-		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
+// 		// Agregar los encabezados CORS a la respuesta HTTP
+// 		w.Header().Set("Access-Control-Allow-Origin", "*")
+// 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT")
+// 		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+// 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		// Terminar de procesar la solicitud
-		handler.ServeHTTP(w, r)
-	}
-}
+// 		// Terminar de procesar la solicitud
+// 		handler.ServeHTTP(w, r)
+// 	}
+// }
--- main.go
+++ main.go
@@ -6,18 +6,25 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+	})
 
-	router.HandleFunc("/books", corsMiddleware(getAllBooks)).Methods("GET")
-	router.HandleFunc("/books/{params:.+}", corsMiddleware(getBookByAny)).Methods("GET")
-	router.HandleFunc("/facturar", corsMiddleware(Facturar)).Methods("POST")
+	router.Use(c.Handler)
+	router.HandleFunc("/books", getAllBooks).Methods("GET")
+	router.HandleFunc("/books/{params:.+}", getBookByAny).Methods("GET")
+	router.HandleFunc("/facturar", Facturar).Methods("POST")
 
-	router.HandleFunc("/books/{id}/{stock}/{bodega}", corsMiddleware(updateOneBook)).Methods("PUT")
-	router.HandleFunc("/", corsMiddleware(sayHello)).Methods("GET")
+	router.HandleFunc("/books/{id}/{stock}/{bodega}", updateOneBook).Methods("PUT")
+	router.HandleFunc("/", sayHello).Methods("GET")
 
 	// Crear el servidor HTTP
 	server := &http.Server{
