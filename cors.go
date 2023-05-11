f5820f288b1e322a779ab1ef245b155495a754c2
Nandocho8 <ccontrerasvergara@hotmail.cl>
2023-05-10 23:25:01 -0400

    cors modificados

--- cors.go
+++ cors.go
@@ -6,13 +6,27 @@ import (
 	"github.com/rs/cors"
 )
 
+func handleOptions(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT")
+	w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.WriteHeader(http.StatusOK)
+}
+
 func corsMiddleware(handleFunc http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Verificar si es una solicitud OPTIONS
+		if r.Method == "OPTIONS" {
+			handleOptions(w, r)
+			return
+		}
+
 		// Configurar las opciones CORS
 		c := cors.New(cors.Options{
-			AllowedMethods:   []string{"OPTIONS", "GET", "POST", "PUT"},
-			AllowedHeaders:   []string{"Authorization", "Content-Type"},
-			AllowCredentials: true,
+			AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			Debug:          true,
 		})
 
 		// Aplicar las opciones CORS a la solicitud
