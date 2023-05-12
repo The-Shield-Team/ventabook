b2791bcd2be54326d93cba59a64709bc953b2bc7
Nandocho8 <ccontrerasvergara@hotmail.cl>
2023-05-11 20:08:54 -0400

    add gorilla/handlers

--- main.go
+++ main.go
@@ -3,22 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/rs/cors"
 )
 
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
-	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"*"},
-		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-	})
 
-	router.Use(c.Handler)
 	router.HandleFunc("/books", getAllBooks).Methods("GET")
 	router.HandleFunc("/books/{params:.+}", getBookByAny).Methods("GET")
 	router.HandleFunc("/facturar", Facturar).Methods("POST")
@@ -26,15 +19,11 @@ func main() {
 	router.HandleFunc("/books/{id}/{stock}/{bodega}", updateOneBook).Methods("PUT")
 	router.HandleFunc("/", sayHello).Methods("GET")
 
-	// Crear el servidor HTTP
-	server := &http.Server{
-		Handler:      router,
-		Addr:         ":3250",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-
 	// Iniciar el servidor
-	log.Fatal(server.ListenAndServe())
+	log.Fatal(http.ListenAndServe(":3250",
+		handlers.CORS(
+			handlers.AllowedOrigins([]string{"*"}),
+			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+			handlers.AllowedHeaders([]string{"*"}))(router)))
 
 }
